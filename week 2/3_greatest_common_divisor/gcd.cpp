83d5cd3f7b3a71e8f4faafd807e188add60c89e6
AnXelion <kongyei@gmail.com>
2019-10-21 23:29:00 +0800

    Finish Greatest Common Divisor (GCD).

--- week 2/3_greatest_common_divisor/gcd.cpp
+++ week 2/3_greatest_common_divisor/gcd.cpp
@@ -12,9 +12,20 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int gcd_euclidean(int a, int b) {
+  while (a != 0 && b != 0) {
+    if (a > b) a = a % b;
+    else b = b % a;
+  }
+  
+  return (a == 0) ? b : a;
+}
+
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  // std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << gcd_euclidean(a, b) << std::endl;
   return 0;
 }
