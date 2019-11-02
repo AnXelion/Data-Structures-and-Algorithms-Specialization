af9f6e3443034db22c9c978025100ae216ec7d60
AnXelion <kongyei@gmail.com>
2019-11-02 19:41:55 +0800

    Finish Collecting Signatures (Covering Segments).

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.cpp
@@ -9,13 +9,26 @@ struct Segment {
   int start, end;
 };
 
+bool segementsComparison(Segment &a, Segment &b) {
+    return (a.end < b.end);
+}
+
 vector<int> optimal_points(vector<Segment> &segments) {
   vector<int> points;
-  //write your code here
-  for (size_t i = 0; i < segments.size(); ++i) {
-    points.push_back(segments[i].start);
-    points.push_back(segments[i].end);
+  // write your code here
+  // Sort segments according to end points
+  sort(segments.begin(), segments.end(), segementsComparison);
+
+  int point = segments[0].end;  // the first rightmost point
+  points.push_back(point);
+
+  for (int i = 1; i < segments.size(); i++) {
+    if ((point < segments[i].start)) { // the point is not in the segment
+      point = segments[i].end;  // update the point to the rightmost of this segment
+      points.push_back(point);  
+    }
   }
+
   return points;
 }
 
@@ -26,6 +39,7 @@ int main() {
   for (size_t i = 0; i < segments.size(); ++i) {
     std::cin >> segments[i].start >> segments[i].end;
   }
+
   vector<int> points = optimal_points(segments);
   std::cout << points.size() << "\n";
   for (size_t i = 0; i < points.size(); ++i) {
