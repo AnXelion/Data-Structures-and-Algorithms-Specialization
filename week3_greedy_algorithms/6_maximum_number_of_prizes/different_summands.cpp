748a2531f61dd9b6ed7afaa2e739dd29aec8134b
AnXelion <kongyei@gmail.com>
2019-11-02 20:04:11 +0800

    Finish Maximum Number of Prizes.

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -6,6 +6,22 @@ using std::vector;
 vector<int> optimal_summands(int n) {
   vector<int> summands;
   //write your code here
+  summands.push_back(1);
+  int remain = n-1;
+  int index = 0;
+
+  while (remain > 0) {
+    if (remain > summands[index]) { // if remain > summands[index], push back summands[index]+1 to the vector
+      summands.push_back(summands[index] + 1);
+      remain -= summands[++index];  // update remain
+    }
+
+    else {
+      summands[index] += remain;  // plus the rest of remain to the last element
+      remain = 0;
+    }  
+  }
+
   return summands;
 }
 
