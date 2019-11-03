71f8b45298a1da3f27bf6b88816d1ed488fc2e78
AnXelion <kongyei@gmail.com>
2019-11-03 15:29:38 +0800

    Finish Maximum Salary.

--- week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,14 +7,40 @@
 using std::vector;
 using std::string;
 
+bool comparisonDigits (string a, string b) {
+  std::stringstream ret;
+  int int_a, int_b;
+
+  // create ab then cast it to int
+  ret << a;
+  ret << b;
+  ret >> int_a;
+
+  // clear the stringstream to create ba
+  ret.clear();  
+
+  // create ba then cast it to int
+  ret << b;
+  ret << a;
+  ret >> int_b;
+
+  if (int_a > int_b)
+    return true;
+  else 
+    return false;
+}
+
+
 string largest_number(vector<string> a) {
   //write your code here
-  std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
-  }
   string result;
-  ret >> result;
+
+  sort(a.begin(), a.end(), comparisonDigits);
+  
+  for (int i = 0; i < a.size(); i++) {
+    result += a[i];
+  }
+  
   return result;
 }
 
@@ -25,5 +51,8 @@ int main() {
   for (size_t i = 0; i < a.size(); i++) {
     std::cin >> a[i];
   }
+
   std::cout << largest_number(a);
+
+  return 0;
 }
