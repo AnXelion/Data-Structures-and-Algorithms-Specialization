f29d2a458c3c2099672966ffba348ecf3136e04b
AnXelion <kongyei@gmail.com>
2019-11-11 19:39:53 +0800

    Finish Majority Element.

--- Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -8,6 +8,24 @@ int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
+  int mid = (left + right) / 2;
+  int left_element = get_majority_element(a, left, mid - 1);
+  int right_element = get_majority_element(a, mid, right);
+
+  int l_count = 0;
+  for (int i = left; i < right; i++) {
+    if (a[i] == left_element)
+      l_count++;
+  }
+  if (l_count > (right - left) / 2) return left_element;
+
+  int r_count = 0;
+  for (int i = left; i < right; i++) {
+    if (a[i] == right_element)
+      r_count++;
+   }
+  if (r_count > (right - left) / 2) return right_element;
+
   return -1;
 }
 
