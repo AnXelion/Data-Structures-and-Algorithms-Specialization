160f1be8d3f2447943d489799b909ea3cbc9a522
AnXelion <kongyei@gmail.com>
2019-11-26 11:49:55 +0800

    Finish 3-way Partition

--- Algorithmic Toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ Algorithmic Toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -5,17 +5,38 @@
 using std::vector;
 using std::swap;
 
-int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
+
+// int partition2(vector<int> &a, int l, int r) {
+//   int x = a[l];
+//   int j = l;
+//   for (int i = l + 1; i <= r; i++) {
+//     if (a[i] <= x) {
+//       j++;
+//       swap(a[i], a[j]);
+//     }
+//   }
+//   swap(a[l], a[j]);
+//   return j;
+// }
+
+vector<int> partition3(vector<int> &a, int l, int r) {
+  int pivot = a[l];
+  int j = l, t = r;
+
+  for (int i = l; i <= t; i++) {
+    if (a[i] < pivot) {
       swap(a[i], a[j]);
+      j++;
     }
-  }
-  swap(a[l], a[j]);
-  return j;
+
+    else if (a[i] > pivot) {
+      swap(a[i], a[t]);
+      t--;
+      i--;
+    }
+  }  
+  // swap(a[l], a[j]);  // Don't need this in 3-way partition compared to 2-way partition.
+  return vector<int> {j,t};
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -25,11 +46,25 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+  vector<int> p = partition3(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  randomized_quick_sort(a, l, p[0] - 1);
+  randomized_quick_sort(a, p[1] + 1, r);
 }
+
+// // version with partition2 
+// void randomized_quick_sort(vector<int> &a, int l, int r) {
+//   if (l >= r) {
+//     return;
+//   }
+
+//   int k = l + rand() % (r - l + 1);
+//   swap(a[l], a[k]);
+//   int m = partition2(a, l, r);
+
+//   randomized_quick_sort(a, l, m - 1);
+//   randomized_quick_sort(a, m + 1, r);
+// }
 
 int main() {
   int n;
